333039e8a7abbd5de14bfd6095c23010cdccba17
Morgan Laco <morgan@codeschool.com>
2015-07-30 11:11:54 -0400

    Implement descending diagonals

--- app/connect4/matches/diagonal.js
+++ app/connect4/matches/diagonal.js
@@ -27,7 +27,8 @@ function isTopRight(grid) {
       numDiags = numCols + numRows - 1,
       found,
       foundPiece,
-      matchReq = 4; // Because Connect 4
+      matchReq = 4, // Because Connect 4
+      col;
 
     // Here, we take successive diagonals, defined by the location of their "base",
     // meaning the column where they meet the ground.
@@ -38,11 +39,10 @@ function isTopRight(grid) {
       found = 0;
       foundPiece = 0;
 
+      col = baseCol;
+
       // Here we work our way up the current diagonal
       for(var row=0; row<numRows; row++) {
-
-        // For each row, we find the column that the diagonal runs through
-        col = baseCol + row;
 
         // Ensure that the given column and row are on the board
         if(col >= 0 && col < numCols && row >= 0 && row < numRows) {
@@ -57,6 +57,7 @@ function isTopRight(grid) {
             return true;
           }
         }
+        ++col; // increase column for next cell
 
       }
   }
@@ -71,43 +72,45 @@ function isTopRight(grid) {
  * @return {Boolean}
  */
 function isTopLeft(grid) {
+  var numCols = 7,
+      numRows = 6,
+      maxDiagSize = Math.min(numCols, numRows),
+      numDiags = numCols + numRows - 1,
+      found,
+      foundPiece,
+      matchReq = 4, // Because Connect 4
+      col;
 
-  let found = 0;
-  let foundPiece = 0;
+    // Here, we take successive diagonals, defined by the location of their "base",
+    // meaning the column where they meet the ground.
+    // The initial baseCol is a negative number, representing that the diagonal starts off
+    // the board. These diagonals intersect the board, nonetheless.
+    for(var baseCol=1-numRows + (matchReq-1); baseCol<numCols - (matchReq - 1); baseCol++) {
 
-  for (let x = 6; x >= 0; x--) {
-    for (let y = 0; y < 6; y++) {
-
-      // Reset
       found = 0;
       foundPiece = 0;
 
-      for (let z = 0; z < 4; z++) {
+      col = baseCol;
 
-        // Undefined or zero? Break loop (impossible match)
-        if (!(grid[x - z] && grid[x - z][y + z])) {
-          break;
+      // Here we work our way *down* the current diagonal
+      for(var row=numRows-1; row>=0; row--) {
+
+        // Ensure that the given column and row are on the board
+        if(col >= 0 && col < numCols && row >= 0 && row < numRows) {
+          let piece = grid[col][row]
+          if(!foundPiece) {
+            foundPiece = piece;
+          }
+          if(piece !== foundPiece) {
+            break;
+          }
+          if((++found) === 4) {
+            return true;
+          }
         }
+        ++col; // increase column for next cell
 
-        let piece = grid[x - z][y + z];
-
-        // If no pieces were previously found, then set the current piece as it
-        if (!foundPiece) {
-          foundPiece = piece;
-        }
-
-        // If piece is not equal to foundPiece, break whole flow
-        if (piece !== foundPiece) {
-          break;
-        }
-
-        // Found 4?
-        if ((++found) === 4) {
-          return true;
-        }
       }
-    }
   }
-
   return false;
 }
