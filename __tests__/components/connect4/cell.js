0c3d1d106afefc9350bfce6789f33ddbe04b0fc8
Sergio Cruz <hello@sergiocruz.me>
2015-08-03 23:07:23 -0400

    Renamed tests back to correct names

---
+++ __tests__/components/connect4/cell.js
@@ -0,0 +1,57 @@
+// jest.dontMock('../../../app/components/connect4/board');
+// jest.dontMock('../../../app/components/connect4/lib');
+
+// import React from 'react/addons';
+// import Board from '../../../app/components/connect4/board';
+// import Connect4 from '../../../app/components/connect4/lib';
+
+
+// const TestUtils = React.addons.TestUtils;
+// let board = new Connect4.Board();
+// let boardMock = jasmine.createSpyObj('boardMock', ['handleAddPiece']);
+
+
+// describe('Cell', () => {
+
+//   let cell;
+//   let nextPlayer;
+
+//   beforeEach(() => {
+
+//     // Finds first cell in the board
+//     cell = boardNode.children[0].children[0];
+//     spyOn(board, 'addPiece').andCallThrough();
+
+//     // First cell from the first column in the board
+//     nextPlayer = board.nextPlayer;
+
+//   });
+
+
+//   it('should call board.addPiece', () => {
+
+//     TestUtils.Simulate.click(cell);
+
+//     // Should have been called with first colum
+//     expect(board.addPiece).toHaveBeenCalledWith(0, nextPlayer);
+//     expect(boardMock.handleGameRefresh).toHaveBeenCalled();
+
+//   });
+
+//   it('should have a different player', () => {
+//     TestUtils.Simulate.click(cell);
+//     expect(board.nextPlayer !== nextPlayer).toBeTruthy();
+//   });
+
+//   it('should not call addPiece when board is inactive', () => {
+
+//     board.isActive = false;
+
+//     TestUtils.Simulate.click(cell);
+
+//     expect(board.addPiece).not.toHaveBeenCalled();
+
+//     board.isActive = true;
+//   });
+
+// });
